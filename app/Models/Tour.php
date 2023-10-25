709506c976d0f22576ab5897b5b07df892f403aa
Wagner Bugs <wagnerbugs@gmail.com>
2023-10-24 23:16:44 -0300

    test: Tour Test + notes about alternative $perPage

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -11,6 +11,10 @@ class Tour extends Model
 {
     use HasFactory, HasUuids;
 
+    // Easyly method recomend to create a config('app.paginationPerPage.tours')
+    // Alternative to define a number of paginate() to model
+    // protected $perPage = 10;
+
     protected $fillable = [
         'travel_id',
         'name',
---
+++ tests/Feature/ToursListTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ToursListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_tours_list_by_travel_slug_returns_correct_tours(): void
+    {
+        $travel = Travel::factory()->create();
+        $tour = Tour::factory()->create(['travel_id' => $travel->id]);
+
+        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => $tour->id]);
+    }
+
+    public function test_tours_price_is_shown_correctly(): void
+    {
+        $travel = Travel::factory()->create();
+        Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 123.45,
+        ]);
+
+        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['price' => '123.45']);
+    }
+
+    public function test_tours_list_returns_pagination(): void
+    {
+        // Alternative way to get the number per page
+        // $toursPerPage = config('app.paginationPerPage.tours');
+
+        // $travel = Travel::factory()->create();
+        // Tour::factory($toursPerPage + 1)->create([
+        //     'travel_id' => $travel->id,
+        // ]);
+
+        $travel = Travel::factory()->create();
+        Tour::factory(16)->create(['travel_id' => $travel->id]);
+
+        $response = $this->get('/api/v1/travels/'.$travel->slug.'/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonPath('meta.last_page', 2);
+    }
+}
