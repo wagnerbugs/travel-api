25a092338a423fef50578be19751c00c753985fb
Wagner Bugs <wagnerbugs@gmail.com>
2023-10-25 05:05:06 -0300

    route: Travel Update + add Group middleware

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,15 @@ Route::get('travels', [TravelController::class, 'index']);
 
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 
-Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::post('travels', [Admin\TravelController::class, 'store']);
-    Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
+Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
+
+    Route::middleware('role:admin')->group(function () {
+        Route::post('travels', [Admin\TravelController::class, 'store']);
+        Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
+    });
+
+    Route::put('travels/{travel}', [Admin\TravelController::class, 'update']);
+
 });
 
 Route::post('login', LoginController::class);
