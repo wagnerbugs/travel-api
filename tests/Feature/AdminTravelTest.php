53544e4d1d4c36128263c415a2cee2142f1125e9
Wagner Bugs <wagnerbugs@gmail.com>
2023-10-25 05:05:20 -0300

    test: Travel Updated

--- tests/Feature/AdminTravelTest.php
+++ tests/Feature/AdminTravelTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Role;
+use App\Models\Travel;
 use App\Models\User;
 use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -52,5 +53,30 @@ class AdminTravelTest extends TestCase
         $response->assertStatus(201);
         $response = $this->get('/api/v1/travels');
         $response->assertJsonFragment(['name' => 'Test Travel']);
+    }
+
+    public function test_update_travel_successfully_with_valid_data(): void
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()->create();
+        $user->roles()->attach(Role::where('name', 'editor')->value('id'));
+        $travel = Travel::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/admin/travels/'.$travel->id, [
+            'name' => 'Test Travel',
+        ]);
+
+        $response->assertStatus(422);
+
+        $response = $this->actingAs($user)->putJson('/api/v1/admin/travels/'.$travel->id, [
+            'name' => 'Test Travel updated',
+            'is_public' => 1,
+            'description' => 'Test Travel Description updated',
+            'number_of_days' => 5,
+        ]);
+
+        $response->assertStatus(200);
+        $response = $this->get('/api/v1/travels');
+        $response->assertJsonFragment(['name' => 'Test Travel updated']);
     }
 }
